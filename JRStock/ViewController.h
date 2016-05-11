c2115f93c6eae05e00a117812461afb440f79f4d
LeeBigRay <lirui5216@126.com>
2016-05-11 09:12:41 +0800

    Initial Commit

---
+++ JRStock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRStock
+//
+//  Created by Meng Chen on 16/5/11.
+//  Copyright © 2016年 LeeRay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JRStock/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JRStock
+//
+//  Created by Meng Chen on 16/5/11.
+//  Copyright © 2016年 LeeRay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
